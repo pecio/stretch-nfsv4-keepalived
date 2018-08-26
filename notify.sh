a3c6c804f2cb647bce8592608d0272479e4bc97a
Raúl Pedroche Novillo <r.pedroche.novillo@gmail.com>
2018-08-26 13:46:18 +0200

    Change NFS from active/pasive to active/active
    The new approach is to have NFS server already started, then
    make Keepalived fail on NFS unavailability.
    
    Previously Keepalived started and stopped the NFS service on
    state change without further monitoring the service status.

--- notify.sh
+++ notify.sh
@@ -2,12 +2,20 @@
 /usr/bin/logger "Invoked notify.sh with $1 $2 $3"
 case "$3" in
   "MASTER")
-    /bin/systemctl start nfs-server
+    /bin/systemctl restart nfs-server.service
   ;;
   "BACKUP")
-    /bin/systemctl stop nfs-server
+    :
   ;;
   "FAULT")
-    /bin/systemctl stop nfs-server
+    # This can be a detected NFS server failure: we want to restart
+    /usr/bin/logger "Checking if NFS server needs restart"
+    sleep 10
+    /bin/systemctl status nfs-server.service
+    if [ $? -ne 0 ]
+    then
+      /usr/bin/logger "Restarting NFS server"
+      /bin/systemctl start nfs-server.service
+    fi
   ;;
 esac
